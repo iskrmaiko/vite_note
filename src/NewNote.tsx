b2783f51d9688e4c394db723218385c5a65091c5
iskrmaiko <116249438+iskrmaiko@users.noreply.github.com>
2023-07-29 05:16:47 -0700

    Add files via upload
    first commit

---
+++ src/NewNote.tsx
@@ -0,0 +1,10 @@
+import { NoteForm } from "./NoteForm"
+
+export const NewNote = () => {
+  return (
+    <div>
+        <h1 className="text-2xl font-bold">New note</h1>
+        <NoteForm />
+    </div>
+  )
+}
